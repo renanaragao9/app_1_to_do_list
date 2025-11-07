c2000cdfd6c082e1feea53d3680c9d243b957ff8
renanaragao9 <renanaragao159@gmail.com>
2025-11-07 16:33:57 -0300

    [FTR] - 99

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_one/screens/home_screen.dart';
 import 'package:project_one/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -24,7 +26,28 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: RoutesScreen(),
     );
   }
+}
+
+class RoutesScreen extends StatelessWidget {
+  const RoutesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream: FirebaseAuth.instance.userChanges(), builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(
+          child: CircularProgressIndicator()
+        );
+      } else {
+        if (snapshot.hasData) {
+          return HomeScreen(user: snapshot.data!);
+        } else {
+          return LoginScreen();
+        }
+      }
+      });
+    }
 }
