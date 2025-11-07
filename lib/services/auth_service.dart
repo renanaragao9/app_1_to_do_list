96029c4fd1852f6c11bda72a17f04dc8ef37619d
renanaragao9 <renanaragao159@gmail.com>
2025-11-07 15:42:57 -0300

    [FTR] AULA - 94

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<String?> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'user-not-found':
+          return 'Usuário não encontrado.';
+        case 'wrong-password':
+          return 'Senha incorreta.';
+      }
+
+      return e.code;
+    }
+
+    return null;
+  }
+
+  Future<String?> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      await userCredential.user!.updateDisplayName(name);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'email-already-in-use':
+          return 'E-mail já está em uso.';
+        case 'weak-password':
+          return 'Senha muito fraca.';
+      }
+
+      return e.code;
+    }
+
+    return null;
+  }
+
+  Future<String?> resetPassword({
+    required String email,
+  }) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'user-not-found':
+          return 'Usuário não encontrado.';
+      }
+
+      return e.code;
+    }
+
+    return null;
+  }
+}
